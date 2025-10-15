c8c015595099494f5e7fbb964d68e72bc893ac47
pablo-barbosa <pablo.barbosa@vision360.com.br>
2025-10-15 18:11:35 -0300

    adicionado notificação funcional no android 15

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import BackgroundFetch from 'react-native-background-fetch';
 import { useEffect } from 'react';
+import BackgroundFetch from 'react-native-background-fetch';
+import { Notifications } from 'react-native-notifications';
 
 export default function App() {
   useEffect(() => {
     const init = async () => {
       const status = await BackgroundFetch.configure(
         {
-          minimumFetchInterval: 15,
+          minimumFetchInterval: 1, // intervalo em minutos
           stopOnTerminate: false,
           enableHeadless: true,
           startOnBoot: true,
@@ -15,22 +16,43 @@ export default function App() {
         async (taskId) => {
           console.log('[BackgroundFetch] Tarefa iniciada:', taskId);
 
+          // 🕒 Pega o horário atual formatado
+          const agora = new Date().toLocaleTimeString();
+
+          // 🔔 Exibe uma notificação com som e vibração
+          Notifications.postLocalNotification({
+            title: 'Tarefa em segundo plano',
+            body: `Executada às ${agora}`,
+            sound: 'default',
+            //@ts-ignore
+            silent: false,
+            category: 'TASK_EXECUTED',
+            userInfo: { taskId },
+          });
+
           // Simula processamento de 1 minuto
           await new Promise((resolve) => setTimeout(resolve, 60000));
 
           console.log('[BackgroundFetch] Tarefa finalizada:', taskId);
-
-          // Finaliza o fetch
           BackgroundFetch.finish(taskId);
         },
         (error) => {
-          console.error('[BackgroundFetch] erro', error);
+          console.error('[BackgroundFetch] Erro', error);
         }
       );
+
+      BackgroundFetch.scheduleTask({
+        taskId: 'com.teste.fetch',
+        delay: 5000, // em ms (5 segundos)
+        forceAlarmManager: true,
+        periodic: true, // executa só uma vez
+      });
 
       console.log('[BackgroundFetch] status', status);
     };
 
+    // 🚀 Inicializa e pede permissão de notificação
+    Notifications.registerRemoteNotifications();
     init();
   }, []);
 
